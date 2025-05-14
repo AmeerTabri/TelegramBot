dc2fb2fad1f076e2bbed83e18ecfde66d254b5fe
ameertabri <ameer.t.2000@gmail.com>
2025-05-15 00:01:08 +0300

    testing actions

--- polybot/img_proc.py
+++ polybot/img_proc.py
@@ -147,7 +147,7 @@ class Img:
                         self.data[x][y] = avg
 
     def predict(self):
-        url = "http://44.244.218.74:8080/predict"
+        url = "http://34.213.168.160:8080/predict"
         with open(self.path, 'rb') as image_file:
             files = {'file': image_file}
             try:
