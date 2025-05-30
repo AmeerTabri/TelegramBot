9633a0319ac301981370a6d30e26a26ebfa6e5a9
ameertabri <ameer.t.2000@gmail.com>
2025-05-30 22:43:03 +0300

    nginx

--- polybot/app.py
+++ polybot/app.py
@@ -22,29 +22,14 @@ def webhook():
     return 'Ok'
 
 
-# if __name__ == "__main__":
-#     cert_path = "/home/ubuntu/TelegramBot/polybot.crt"
-#     bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL)
-#
-#     # Set webhook with certificate
-#     bot.telegram_bot_client.set_webhook(
-#         url=f"{BOT_APP_URL}/{TELEGRAM_BOT_TOKEN}/",
-#         certificate=open(cert_path, "r")
-#     )
-#
-#     app.run(host='0.0.0.0', port=8443)
-
-
 if __name__ == "__main__":
-    bot = ImageProcessingBot(
-        "7663738647:AAHiDJ75yGt89pFzVWkNp1kLCHwViM9egAo",
-        "https://52.36.176.111"
-    )
+    cert_path = "/home/ubuntu/TelegramBot/polybot.crt"
+    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL)
 
     # Set webhook with certificate
     bot.telegram_bot_client.set_webhook(
-        url="https://52.36.176.111/7663738647:AAHiDJ75yGt89pFzVWkNp1kLCHwViM9egAo/",
-        certificate=open("/home/ubuntu/TelegramBot/polybot.crt", "r")
+        url=f"{BOT_APP_URL}/{TELEGRAM_BOT_TOKEN}/",
+        certificate=open(cert_path, "r")
     )
 
     app.run(host='0.0.0.0', port=8443)
