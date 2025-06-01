49c6b96fa7df305809a4af3cc1391f8a6b84f155
ameertabri <ameer.t.2000@gmail.com>
2025-06-01 19:11:15 +0300

    s3 feature

--- polybot/s3.py
+++ polybot/s3.py
@@ -1,14 +1,8 @@
 import os
 import boto3
 
-<<<<<<< HEAD
 AWS_REGION = os.environ["AWS_REGION"]
 AWS_S3_BUCKET = os.environ["AWS_S3_BUCKET"]
-=======
-AWS_REGION = "us-east-1"
-AWS_S3_BUCKET = "ameer-polybot-images-dev"
-
->>>>>>> feature/user_upload_images_to_s3
 
 s3 = boto3.client('s3', region_name=AWS_REGION)
 
@@ -19,8 +13,4 @@ def upload_image_to_s3(local_path, s3_key):
 
 def download_predicted_image_from_s3(chat_id: str, image_name: str, local_path: str):
     s3_key = f"{chat_id}/predicted/{image_name}"
-<<<<<<< HEAD
     s3.download_file(AWS_S3_BUCKET, s3_key, local_path)
-=======
-    s3.download_file(AWS_S3_BUCKET, s3_key, local_path)
->>>>>>> feature/user_upload_images_to_s3
